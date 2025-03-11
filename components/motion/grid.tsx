1c65351a979ef6751d71df15ef480a44de417b92
in-deepaknegi <negideepak.in@gmail.com>
2025-03-12 00:53:05 +0530

    fix: fix for production

--- components/motion/grid.tsx
+++ components/motion/grid.tsx
@@ -10,7 +10,12 @@ interface GridProps {
 
 export const Grid = ({ children, className }: GridProps) => {
     return (
-        <div className={twMerge("bg-zinc-900 px-4 py-12 text-zinc-50", className)}>
+        <div
+            className={twMerge(
+                "bg-zinc-900 px-4 py-12 text-zinc-50",
+                className,
+            )}
+        >
             <motion.div
                 initial="initial"
                 animate="animate"
@@ -23,6 +28,7 @@ export const Grid = ({ children, className }: GridProps) => {
     );
 };
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const Block = ({ className, ...rest }: any) => {
     return (
         <motion.div
@@ -30,10 +36,15 @@ export const Block = ({ className, ...rest }: any) => {
                 initial: { scale: 0.5, y: 50, opacity: 0 },
                 animate: { scale: 1, y: 0, opacity: 1 },
             }}
-            transition={{ type: "spring", mass: 3, stiffness: 400, damping: 50 }}
+            transition={{
+                type: "spring",
+                mass: 3,
+                stiffness: 400,
+                damping: 50,
+            }}
             className={twMerge(
                 "col-span-4 rounded-lg border border-zinc-700 bg-zinc-800 p-6",
-                className
+                className,
             )}
             {...rest}
         />
